7bc43c72aca5419283f390ae9a944b498d283156
Daniel Braga <daniel.braga@shibata.com.br>
2015-11-13 00:13:33 -0200

    added CORS configuration

--- example/config/extdirect.php
+++ example/config/extdirect.php
@@ -2,21 +2,42 @@
 return [
     'discoverer' => [
         'paths' => [
-            __DIR__ . '/../src'
+            __DIR__ . '/../src',
         ]
     ],
     'cache' => [
         'directory' => __DIR__ . '/../cache',
-        'lifetime' => 1800
+        'lifetime' => 60,
+    ],
+    'cors' => [
+        'requestAllowedOrigins' => [
+            'http://localhost:8080' => true,
+        ],
+        'requestAllowedMethods' => [
+            'GET' => true,
+            'OPTIONS' => true,
+            'POST' => true
+        ],
+        'requestAllowedHeaders' => [
+            'content-type'     => true,
+            'x-requested-with' => true,
+            'origin'           => true,
+        ],
+        'requestCredentialsSupported' => true,
+        'serverOrigin' => [
+            'scheme' => 'http',
+            'host'   => 'localhost',
+            'port'   => '8081',
+        ]
     ],
     'api' => [
         'descriptor' => 'window.uERP_REMOTING_API',
         'declaration' => [
-            'url' => 'localhost:8080/router.php',
+            'url' => 'http://localhost:8081/router.php',
             'type' => 'remoting',
             'id' => 'uERP', // it's required for the cache mechanism
             'namespace' => 'Ext.php',
-            'timeout' => null
+            'timeout' => null,
         ]
     ]
 ];
