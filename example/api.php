e4539231d7363ebd564c5cd24384a3b85fbbd9db
Daniel Braga <daniel.braga@shibata.com.br>
2015-11-13 15:54:24 -0200

    date_default_timezone_set() is not required here

--- example/api.php
+++ example/api.php
@@ -1,6 +1,4 @@
 <?php
-date_default_timezone_set('America/Sao_Paulo');
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $config = new ExtDirect\Config(include __DIR__ . '/config/extdirect.php');
